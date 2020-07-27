5d90c6d69202f9c122442abd85de7f6e123202be
João Pedro Matias <jpmmatias@gmail.com>
2020-07-27 01:28:02 -0300

    first deploy test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const users = require('./routes/users');
 const contacts = require('./routes/contacts');
 const auth = require('./routes/auth');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +12,15 @@ connectDB();
 
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.json({msg: 'Wellcome to the Kep API'}));
-
 app.use('/api/users', users);
 app.use('/api/contacts', contacts);
 app.use('/api/auth', auth);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
 app.listen(PORT, () => console.log(`Listining at port ${PORT}`));
